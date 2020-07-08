5e9a252b76bf1636e2726928a179f428f3add941
Danilo Vilhena <danilo.vilhena@edu.unifor.br>
2020-07-08 14:56:49 -0300

    Change font size implementation

--- src/app/components/acessibility-bar/acessibility-bar.component.ts
+++ src/app/components/acessibility-bar/acessibility-bar.component.ts
@@ -6,9 +6,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./acessibility-bar.component.css']
 })
 export class AcessibilityBarComponent {
-  constructor() { }
+  bodySize = 16;
+  h3Size = 28;
+  h4Size = 24;
+  h5Size = 20;
+  searchSize = 20;
 
   toggleHighContrast() {
     document.querySelector('html').classList.toggle('contrast')
+  }
+
+  increaseFontSize() {
+    this.bodySize = this.bodySize + 1
+    this.h3Size = this.h3Size + 1
+    this.h4Size = this.h4Size + 1
+    this.h5Size = this.h5Size + 1
+    this.searchSize = this.searchSize + 1
+    this.setCurrentFontSize()
+  }
+
+  returnToInitialFontSize() {
+    this.bodySize = 16
+    this.h3Size = 28
+    this.h4Size = 24
+    this.h5Size = 20
+    this.searchSize = 20
+    this.setCurrentFontSize()
+  }
+
+  decreaseFontSize() {
+    this.bodySize = this.bodySize - 1
+    this.h3Size = this.h3Size - 1
+    this.h4Size = this.h4Size - 1
+    this.h5Size = this.h5Size - 1
+    this.searchSize = this.searchSize - 1
+    this.setCurrentFontSize()
+  }
+
+  setCurrentFontSize() {
+    document.querySelector('body').style.fontSize = this.bodySize + "px"
+
+    document.querySelectorAll('h3').forEach((element) => {
+      element.style.fontSize = this.h3Size + "px"
+    })
+
+    document.querySelectorAll('h5').forEach((element) => {
+      element.style.fontSize = this.h5Size + "px"
+    })
+
+    document.querySelectorAll('h4').forEach((element) => {
+      element.style.fontSize = this.h4Size + "px"
+    })
+
+    document.querySelectorAll('button').forEach((element) => {
+      element.style.fontSize = this.searchSize + "px"
+    })
+
+    document.querySelectorAll('input').forEach((element) => {
+      element.style.fontSize = this.searchSize + "px"
+    })
   }
 }
